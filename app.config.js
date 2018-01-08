f46c5f3ff9724711ca58692752ad2fa45bb967be
liyuan.meng <34151318+liyuan-meng@users.noreply.github.com>
2018-01-09 00:13:08 +0800

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -6,16 +6,16 @@ angular.module("helloSolarSystem",
     function ($stateProvider) {
         $stateProvider.state('helloState', {
             url: '/helloState',
-            templateUrl: './components/hello/hello.template.html'
+            template:'<hello></hello>'
         }).state('aboutState', {
             url: '/about',
             template: '<h4>Its the UI-Router Hello Solar System app!</h4>'
         }).state('peopleState', {
             url: '/peopleList',
-            templateUrl: './components/people-list/people-list.template.html'
+            template:'<people-list></people-list>'
         }).state('peopleState.details', {
-            url:'^/detail/:id',
-            templateUrl: './components/people-detail/people-detail.template.html'
+            url:'/detail/:id',
+            template: '<people-detail></people-detail>'
         })
     }
 ]);
