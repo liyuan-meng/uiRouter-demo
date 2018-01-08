85dae2fb9646d2d97c7eece53a37ec10ddd03fa3
liyuan.meng <34151318+liyuan-meng@users.noreply.github.com>
2018-01-09 00:14:24 +0800

    Update people-list.component.js

--- components/people-list/people-list.component.js
+++ components/people-list/people-list.component.js
@@ -3,9 +3,12 @@
 angular.module("peopleList", ['people.checkPeople'])
     .component('peopleList', {
         templateUrl: './components/people-list/people-list.template.html',
-        controller: ['CheckPeople',
-            function PeopleListController(CheckPeople) {
-                this.people = CheckPeople.getData();
+        controller: ['CheckPeople','$scope',
+            function PeopleListController(CheckPeople, $scope) {
+                $scope.people = [];
+                CheckPeople.getData().then(function(data){
+                    $scope.people = data;
+                });
             }
         ]
     });
